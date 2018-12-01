b26b438d7ebd51476bb55562154891853056757e
shotaro-ooe <shoooe97@gmail.com>
2018-12-01 16:25:50 +0900

    おみくじアプリ完成

--- flask_sample02.py
+++ flask_sample02.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
+items = ["大吉","吉","中吉","小吉","大凶"]
 
 @app.route("/")
 def index():
@@ -10,6 +12,13 @@ def index():
 @app.route("/user/<name>")
 def hello(name):
     return render_template("hello.html",name=name)
+
+@app.route("/omikuji/<name>")
+def omikuji(name):
+    item=random.choice(items)
+    return render_template("omikuji.html",item=item,name=name)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
